82e68fddde3bb3b4a372fa76522372f6b7d7db90
Jason Pompon <jasonpompon@gmail.com>
2017-12-13 03:48:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function theBeatlesPlay(musiciansArr, instrumentsArr) {
     let strings = [];
-    for (let i = 0; i < musicians.length; i++) {
-        strings.push(`${musicians[i]} plays ${instruments[i]}`)
+    for (let i = 0; i < musiciansArr.length; i++) {
+        strings.push(`${musiciansArr[i]} plays ${instrumentsArr[i]}`)
     }
 
     return strings
 }
 
 function johnLennonFacts(factsArr) {
-    
+    let i = 0;
+    while (i < factsArr.length) {
+        console.log(factsArr[i]);
+        i++
+    }
+    return factsArr
 }
