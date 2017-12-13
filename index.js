089c4ec4a1de875c947d61532256032dd0f2c654
Jason Pompon <jasonpompon@gmail.com>
2017-12-13 03:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musiciansArr, instrumentsArr) {
     let strings = [];
     for (let i = 0; i < musicians.length; i++) {
         strings.push(`${musicians[i]} plays ${instruments[i]}`)
     }
 
     return strings
+}
+
+function johnLennonFacts(factsArr) {
+    
 }
