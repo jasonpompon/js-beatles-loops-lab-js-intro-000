e29e65fb7f7af0c546b5eeb9adbaa64de5fc5e36
Jason Pompon <jasonpompon@gmail.com>
2017-12-13 03:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
     let strings = [];
-    
+    for (let i = 0; i < musicians.length; i++) {
+        strings.push(`${musicians[i]} plays ${instruments[0]}`)
+    }
 }
